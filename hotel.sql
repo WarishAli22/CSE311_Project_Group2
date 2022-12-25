ed6931996a381cb289cce018e5c4b94a5c386939
mariaoishee <118234785+mariaoishee@users.noreply.github.com>
2022-12-25 16:19:20 +0600

    update

--- hotel.sql
+++ hotel.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 20, 2022 at 09:54 AM
+-- Generation Time: Dec 25, 2022 at 11:18 AM
 -- Server version: 10.4.25-MariaDB
--- PHP Version: 8.0.23
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,11 +28,19 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admin` (
-  `Email` varchar(150) NOT NULL,
-  `First_name` varchar(100) NOT NULL,
-  `Last_name` varchar(100) NOT NULL,
-  `PASSWORD` int(25) NOT NULL
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `admin_password` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`first_name`, `last_name`, `email`, `admin_password`) VALUES
+('Maria', 'Oishee', 'maria.oishee@northsouth.edu', 'password2'),
+('Sakib', 'Jawad', 'sakib.jawad@northsouth.edu', 'password1');
 
 -- --------------------------------------------------------
 
@@ -41,13 +49,23 @@ CREATE TABLE `admin` (
 --
 
 CREATE TABLE `booking` (
-  `custID` int(25) NOT NULL,
-  `BookingID` int(50) NOT NULL,
-  `Child_Num` int(20) NOT NULL,
-  `Adult_Num` int(100) NOT NULL,
-  `Start_Date` date DEFAULT NULL,
-  `End_Date` date DEFAULT NULL
+  `booking_id` int(11) NOT NULL,
+  `cid` int(11) DEFAULT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  `child_number` int(11) DEFAULT NULL,
+  `adult_number` int(11) DEFAULT NULL,
+  `room_number` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `booking`
+--
+
+INSERT INTO `booking` (`booking_id`, `cid`, `start_date`, `end_date`, `child_number`, `adult_number`, `room_number`) VALUES
+(1, 1, '2022-01-01', '2022-01-05', 2, 2, 101),
+(2, 2, '2022-01-06', '2022-01-10', 1, 2, 103),
+(3, 3, '2022-01-11', '2022-01-15', 0, 2, 102);
 
 -- --------------------------------------------------------
 
@@ -56,27 +74,44 @@ CREATE TABLE `booking` (
 --
 
 CREATE TABLE `customer` (
-  `custID` int(25) NOT NULL,
-  `First_name` varchar(255) NOT NULL,
-  `Last_name` varchar(255) NOT NULL,
-  `NID` int(25) NOT NULL,
-  `Phone` int(11) NOT NULL,
-  `Date_of_Birth` date DEFAULT NULL,
-  `Email` varchar(100) DEFAULT NULL,
-  `Password` varchar(16) NOT NULL
+  `cid` int(11) NOT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `nid` varchar(255) DEFAULT NULL,
+  `date_of_birth` date DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `customer`
 --
 
-INSERT INTO `customer` (`custID`, `First_name`, `Last_name`, `NID`, `Phone`, `Date_of_Birth`, `Email`, `Password`) VALUES
-(1, 'Vandegrift', 'Tonny', 90875625, 10654, '1978-01-09', 'vantony@gmail.com', 'vanton123'),
-(2, 'Donne', 'Smith', 90578626, 104536, '1997-05-23', 'donesmith01@gmail.com', 'rparbona345'),
-(3, 'Grod', 'Hoont', 90785627, 13455, '1988-04-04', 'grodhoont1988@gmail.com', 'amijanena123'),
-(4, 'Kaedehara', 'Kazuha', 90758623, 103645, '2000-03-22', 'kazuhara@gmail.com', 'kazuha234'),
-(5, 'Billie', 'Eonny', 90857624, 12453, '1987-01-13', 'billieny2345@gmail.com', 'benny321'),
-(6, 'Brown', 'John', 90587628, 112346, '1967-10-17', 'brownjohn898@gmail.com', 'brown1234');
+INSERT INTO `customer` (`cid`, `first_name`, `last_name`, `email`, `nid`, `date_of_birth`, `password`) VALUES
+(1, 'Mark', 'Doe', 'john.doe@example.com', '1234567890', '1980-01-01', 'password1'),
+(2, 'Jane', 'Doe', 'jane.doe@example.com', '0987654321', '1985-02-14', 'password2'),
+(3, 'Jon', 'Smith', 'bob.smith@example.com', '1111111111', '1990-03-21', 'password3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer_phone`
+--
+
+CREATE TABLE `customer_phone` (
+  `cid` int(11) NOT NULL,
+  `phone` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `customer_phone`
+--
+
+INSERT INTO `customer_phone` (`cid`, `phone`) VALUES
+(1, '123-456-7890'),
+(1, '234-567-8901'),
+(2, '345-678-9012'),
+(3, '456-789-0123');
 
 -- --------------------------------------------------------
 
@@ -85,10 +120,20 @@ INSERT INTO `customer` (`custID`, `First_name`, `Last_name`, `NID`, `Phone`, `Da
 --
 
 CREATE TABLE `room` (
-  `Room_Num` int(25) NOT NULL,
-  `Room_Type` varchar(255) NOT NULL,
-  `Availability` tinyint(1) DEFAULT NULL
+  `room_number` int(11) NOT NULL,
+  `room_type` varchar(255) DEFAULT NULL,
+  `availability` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `room`
+--
+
+INSERT INTO `room` (`room_number`, `room_type`, `availability`) VALUES
+(101, 'Standard', 1),
+(102, 'Standard', 1),
+(103, 'Deluxe', 1),
+(104, 'Suite', 0);
 
 -- --------------------------------------------------------
 
@@ -97,12 +142,22 @@ CREATE TABLE `room` (
 --
 
 CREATE TABLE `staff` (
-  `SID` int(25) NOT NULL,
-  `First_name` varchar(100) NOT NULL,
-  `Last_name` varchar(100) NOT NULL,
-  `age` int(25) NOT NULL,
-  `designation` varchar(100) NOT NULL
+  `sid` int(11) NOT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `designation` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `staff`
+--
+
+INSERT INTO `staff` (`sid`, `first_name`, `last_name`, `email`, `age`, `designation`) VALUES
+(1, 'John', 'Doe', 'john.doe@example.com', 35, 'Manager'),
+(2, 'Jane', 'Doe', 'jane.doe@example.com', 32, 'Assistant Manager'),
+(3, 'Bob', 'Smith', 'bob.smith@example.com', 28, 'Front Desk Clerk');
 
 --
 -- Indexes for dumped tables
@@ -112,31 +167,39 @@ CREATE TABLE `staff` (
 -- Indexes for table `admin`
 --
 ALTER TABLE `admin`
-  ADD PRIMARY KEY (`Email`);
+  ADD PRIMARY KEY (`email`);
 
 --
 -- Indexes for table `booking`
 --
 ALTER TABLE `booking`
-  ADD PRIMARY KEY (`custID`,`BookingID`);
+  ADD PRIMARY KEY (`booking_id`),
+  ADD KEY `cid` (`cid`),
+  ADD KEY `room_number` (`room_number`);
 
 --
 -- Indexes for table `customer`
 --
 ALTER TABLE `customer`
-  ADD PRIMARY KEY (`custID`);
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- Indexes for table `customer_phone`
+--
+ALTER TABLE `customer_phone`
+  ADD PRIMARY KEY (`cid`,`phone`);
 
 --
 -- Indexes for table `room`
 --
 ALTER TABLE `room`
-  ADD PRIMARY KEY (`Room_Num`);
+  ADD PRIMARY KEY (`room_number`);
 
 --
 -- Indexes for table `staff`
 --
 ALTER TABLE `staff`
-  ADD PRIMARY KEY (`SID`);
+  ADD PRIMARY KEY (`sid`);
 
 --
 -- Constraints for dumped tables
@@ -146,7 +209,14 @@ ALTER TABLE `staff`
 -- Constraints for table `booking`
 --
 ALTER TABLE `booking`
-  ADD CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`custID`) REFERENCES `customer` (`custID`);
+  ADD CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`cid`) REFERENCES `customer` (`cid`),
+  ADD CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`room_number`) REFERENCES `room` (`room_number`);
+
+--
+-- Constraints for table `customer_phone`
+--
+ALTER TABLE `customer_phone`
+  ADD CONSTRAINT `customer_phone_ibfk_1` FOREIGN KEY (`cid`) REFERENCES `customer` (`cid`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
